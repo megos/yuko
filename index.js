db00d2afb4b92c21e5fab533af80164025fbd069
megos <gmegos.27@gmail.com>
2021-06-14 13:47:08 +0900

    Add toggle button

---
+++ index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.querySelector(".toggle-button");
+  const neon = document.querySelector(".neon");
+
+  const on = () => {
+    neon.classList.add("on");
+    toggleButton.innerHTML = "閉店";
+  };
+  const off = () => {
+    neon.classList.remove("on");
+    toggleButton.innerHTML = "ボォん";
+  };
+
+  let timer = setTimeout(() => {
+    on();
+  }, 2000);
+
+  toggleButton.addEventListener("click", () => {
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+    const isOn = neon.classList.contains("on");
+    if (isOn) off();
+    else on();
+  });
+});
